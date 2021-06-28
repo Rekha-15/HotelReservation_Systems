fe9b0414686101f43b33b772c0be495cde58a23e
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-28 06:41:22 -0400

    add : welcome mesaage : .

---
+++ src/main/java/com/Hotelreservationsystem/HotelReservation.java
@@ -0,0 +1,16 @@
+/************************************************************************************
+ * @purpose Implementing Hotel Reservation System Program
+ * @author Rekha
+ * version 1.0
+ * since 14-06-2021
+ ************************************************************************************/
+package com.Hotelreservationsystem;
+
+public class HotelReservation {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Hotel Reservation System Program");
+	}
+
+}
