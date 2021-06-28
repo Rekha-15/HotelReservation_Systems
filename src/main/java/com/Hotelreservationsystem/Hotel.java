6a7b7d471f55c0c8c401e864f345ee65a1a8197a
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-28 06:56:50 -0400

    add : added week end rates : .

--- src/main/java/com/Hotelreservationsystem/Hotel.java
+++ src/main/java/com/Hotelreservationsystem/Hotel.java
@@ -9,21 +9,24 @@ package com.Hotelreservationsystem;
 public class Hotel {
 	public String name;
 	public int regularWeekDayRate;
+	public int regularWeekEndRate;
 
 	/**
 	 * Created a parameterize constructor of the class hotel .
 	 * 
 	 * @param name
 	 * @param regularWeekDayRate
+	 * @param regularWeekEndRate
 	 */
-	public Hotel(String name, int regularWeekDayRate) {
+	public Hotel( String name, int regularWeekDayRate,int regularWeekEndRate) {
 		this.name = name;
 		this.regularWeekDayRate = regularWeekDayRate;
+		this.regularWeekEndRate = regularWeekEndRate;
 
 	}
 
 	@Override
 	public String toString() {
-		return "Hotel Name: " + name + " " + "\nRegularWeekDayRate: " + regularWeekDayRate;
+		return "Hotel Name: " + name + " " + "\nRegularWeekDayRate: " + regularWeekDayRate + "\nRegularWeekEndRate: " + regularWeekEndRate ;
 	}
 }
--- src/test/java/com/Hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/Hotelreservationsystem/HotelReservationTest.java
@@ -15,47 +15,48 @@ import org.junit.Test;
 public class HotelReservationTest {
 	/**
 	 * Created a parameterized constructor of hotelReservation.
+	 * UC-3 : added the weekend rates for the hotels 
 	 */
-
-	HotelReservation hotelReservation = new HotelReservation();
-	Hotel lakewood = new Hotel("LakeWood", 110);
-	Hotel bridgewood = new Hotel("BridgeWood", 150);
-	Hotel ridgewood = new Hotel("RidgeWood", 220);
-
-	/**
-	 * setup is a function through which i can add the value in the array list. by
-	 * calling the addHotel function , adding the value in array list.
-	 */
-
-	@Before
-	public void setUp() {
 		HotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.addHotel(lakewood);
-		hotelReservation.addHotel(bridgewood);
-		hotelReservation.addHotel(ridgewood);
-	}
+	    Hotel lakewood = new Hotel("LakeWood", 110,90);
+	    Hotel bridgewood = new Hotel("BridgeWood", 150,50);
+	    Hotel ridgewood = new Hotel("RidgeWood", 220,150);
 
-	/**
-	 * This test method is use to check , if the hotel is added the return true .
-	 */
-	@Test
-	public void givenHotel_ToAddInHotelReservation_ShouldReturnTrue() {
-		Assert.assertTrue(HotelReservation.hotels.contains(lakewood));
-		Assert.assertTrue(HotelReservation.hotels.contains(bridgewood));
-		Assert.assertTrue(HotelReservation.hotels.contains(ridgewood));
-		HotelReservation.hotels.forEach(System.out::println);
-	}
+	    /**
+	     * setup is a function through which i can add the value in the array list.
+	     * by calling the addHotel function , adding the value in array list.
+	     */
+	    @Before
+	    public void setUp() 
+	    {
+	    	HotelReservation hotelReservation = new HotelReservation();
+	        hotelReservation.addHotel(lakewood);
+	        hotelReservation.addHotel(bridgewood);
+	        hotelReservation.addHotel(ridgewood);
+	    }
+	    
+	    /**
+	     *This test method is use to check , if the hotel is added the return true . 
+	     */
 
-	/**
-	 * This method is used to check the cheapest hotel between the given data range
-	 * .
-	 */
-	@Test
-	public void givenDateRange_ShouldReturnCheapestHotel() {
-		LocalDate startDate = LocalDate.of(2020, Month.SEPTEMBER, 10);
-		LocalDate lastDate = LocalDate.of(2020, Month.SEPTEMBER, 11);
-		Hotel cheapestHotel = hotelReservation.getCheapestHotel(startDate, lastDate);
-		Assert.assertEquals("LakeWood", cheapestHotel.name);
-	}
-
+	    @Test
+	    public void givenHotel_ToAddInHotelReservation_ShouldReturnTrue() 
+	    {
+			Assert.assertTrue(HotelReservation.hotels.contains(lakewood));
+	        Assert.assertTrue(HotelReservation.hotels.contains(bridgewood));
+	        Assert.assertTrue(HotelReservation.hotels.contains(ridgewood));
+	        HotelReservation.hotels.forEach(System.out::println);
+	    }
+	    
+	    /**
+	     * This method is used to check the cheapest hotel between the given data range .
+	     */
+	    @Test
+	    public void givenDateRange_ShouldReturnCheapestHotel() {
+	        LocalDate startDate = LocalDate.of(2020, Month.SEPTEMBER, 10);
+	        LocalDate lastDate = LocalDate.of(2020, Month.SEPTEMBER, 11);
+	        Hotel cheapestHotel = hotelReservation.getCheapestHotel(startDate, lastDate);
+	        Assert.assertEquals("LakeWood", cheapestHotel.name);
+	    }
+	    	 
 }
